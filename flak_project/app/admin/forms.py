c703599dfbeaeab9153eed1a8a714dfc995236a8
156101zhj <1561014362@qq.com>
2019-03-21 11:42:10 +0800

    flsak在线系统测试完成之后提交

---
+++ flak_project/app/admin/forms.py
@@ -0,0 +1,344 @@
+# coding:utf8
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField,FileField,TextAreaField,SelectField,DateField,SelectMultipleField
+from wtforms.validators import DataRequired, ValidationError, EqualTo
+from app.models import Admin,Tag,Auth,Role
+from manage import app
+
+# 电影添加标签
+with app.app_context():  # 解决RuntimeError: application not registered on db instance and
+    tags = Tag.query.all()
+
+with app.app_context():
+    role_all = Role.query.all()
+
+
+class LoginForm(FlaskForm):
+    """管理员登录表单"""
+    account = StringField(
+        label="账号",
+        # 验证器validator
+        validators=[
+            DataRequired("请输入账号！")
+        ],
+        description="账号",
+        render_kw={
+            "class": "form-control",
+            "placeholder": "请输入账号！",
+            "required": "required"
+        }
+    )
+    # 定义密码框
+    pwd = PasswordField(
+        label="密码",
+        validators=[
+            DataRequired("请输入密码！")
+        ],
+        # 描述与样式属性值
+        description="密码",
+        render_kw={
+            "class": "form-control",
+            "placeholder": "请输入密码！",
+            "required": "required"
+        }
+    )
+    # 登录按钮
+    submit = SubmitField(
+        label="登录",
+        render_kw={
+            "class": "btn btn-primary btn-block btn-flat"
+        }
+    )
+
+    def validate_account(self, field):
+        account = field.data
+        admin = Admin.query.filter_by(name=account).count()
+        if admin == 0:
+            raise ValidationError("账号不存在")
+
+class TagForm(FlaskForm):
+    name = StringField(
+        label= "名称",
+        validators=[
+            DataRequired("请输入标签！")
+        ],
+        description="标签",
+        render_kw={
+            "class": "form-control",
+            "id":"input_name",
+            "placeholder": "请输入标签名称！"
+        }
+    )
+
+    submit = SubmitField(
+        label="编辑",
+        render_kw={
+            "class": "btn btn-primary",
+        }
+    )
+
+class MovieForm(FlaskForm):
+    """
+    电影表单
+    """
+    title = StringField(
+        label="片名",
+        validators=[
+            DataRequired("请输入片名！")
+        ],
+        description="片名",
+        render_kw={
+            "class": "form-control",
+            "id": "input_title",
+            "placeholder": "请输入片名！"
+        }
+    )
+    url = FileField(
+        label="文件",
+        validators=[
+            DataRequired("请上传文件！")
+        ],
+        description="文件",
+    )
+    info = TextAreaField(
+        label="简介",
+        validators=[
+            DataRequired("请输入简介！")
+        ],
+        description="简介",
+        render_kw={
+            "class": "form-control",
+            "id": "input_info",
+            "rows": 10
+        }
+    )
+    logo = FileField(
+        label="封面",
+        validators=[
+            DataRequired("请上传封面！")
+        ],
+        description="封面",
+    )
+    star = SelectField(
+        label="星级",
+        validators=[
+            DataRequired("请选择星级！")
+        ],
+        coerce=int,
+        choices=[(1, "1星"), (2, "2星"), (3, "3星"), (4, "4星"), (5, "5星")],
+        description="星级",
+        render_kw={
+            "class": "form-control",
+            "id": "input_star"
+        }
+    )
+    tag_id = SelectField(
+        label="标签",
+        validators=[
+            DataRequired("请选择标签！")
+        ],
+        coerce=int,
+        choices=[(v.id, v.name) for v in tags],
+        description="标签",
+        render_kw={
+            "class": "form-control",
+            "id": "input_tag_id"
+        }
+    )
+    area = StringField(
+        label="地区",
+        validators=[
+            DataRequired("请输入地区！")
+        ],
+        description="地区",
+        render_kw={
+            "class": "form-control",
+            "id": "input_area",
+            "placeholder": "请输入地区！"
+        }
+    )
+    length = StringField(
+        label="片长",
+        validators=[
+            DataRequired("请输入片长！")
+        ],
+        description="片长",
+        render_kw={
+            "class": "form-control",
+            "id": "input_length",
+            "placeholder": "请输入片长！"
+        }
+    )
+    release_time = DateField(
+        label="上映时间",
+        validators=[
+            DataRequired("请选择上映时间！")
+        ],
+        description="上映时间",
+        render_kw={
+            "class": "form-control",
+            "placeholder": "请选择上映时间！",
+            "id": "input_release_time"
+        }
+    )
+    submit = SubmitField(
+        label="编辑",
+        render_kw={
+            "class": "btn btn-primary",
+        }
+    )
+
+class PreviewForm(FlaskForm):
+    """
+    预告表单
+    """
+    title = StringField(
+        label="预告标题",
+        validators=[
+            DataRequired("请输入预告标题！")
+        ],
+        description="预告标题",
+        render_kw={
+            "class": "form-control",
+            "placeholder": "请输入预告标题！"
+        }
+    )
+    logo = FileField(
+        label="预告封面",
+        validators=[
+            DataRequired("请上传预告封面！")
+        ],
+        description="预告封面",
+    )
+    submit = SubmitField(
+        label='编辑',
+        render_kw={
+            "class": "btn btn-primary",
+        }
+    )
+class AuthForm(FlaskForm):
+    """
+    权限表单
+    """
+    name = StringField(
+        label="权限名称",
+        validators=[
+            DataRequired("请输入权限名称！")
+        ],
+        description="权限名称",
+        render_kw={
+            "class": "form-control",
+            "placeholder": "请输入权限名称！"
+        }
+    )
+    url = StringField(
+        label="权限地址",
+        validators=[
+            DataRequired("请输入权限地址！")
+        ],
+        description="权限地址",
+        render_kw={
+            "class": "form-control",
+            "placeholder": "请输入权限地址！"
+        }
+    )
+    submit = SubmitField(
+        label='编辑',
+        render_kw={
+            "class": "btn btn-primary",
+        }
+    )
+
+# 为RoleForm获取auth数据：需要获得当前应用上下文的db
+with app.app_context():
+    auth_all = Auth.query.all()
+
+class RoleForm(FlaskForm):
+    """
+    角色表单
+    """
+    name = StringField(
+        label="角色名称",
+        validators=[
+            DataRequired("请输入角色名称！")
+        ],
+        description="角色名称",
+        render_kw={
+            "class": "form-control",
+            "placeholder": "请输入角色名称！"
+        }
+    )
+    auths = SelectMultipleField(
+        label="权限列表",
+        validators=[
+            DataRequired("请选择权限列表！")
+        ],
+        coerce=int,
+        choices=[(v.id, v.name) for v in auth_all],
+        description="权限列表",
+        render_kw={
+            "class": "form-control",
+        }
+    )
+    submit = SubmitField(
+        '编辑',
+        render_kw={
+            "class": "btn btn-primary",
+        }
+    )
+
+class AdminForm(FlaskForm):
+    """
+    管理员表单
+    """
+    name = StringField(
+        label="管理员名称",
+        validators=[
+            DataRequired("请输入管理员名称！")
+        ],
+        description="管理员名称",
+        render_kw={
+            "class": "form-control",
+            "placeholder": "请输入管理员名称！",
+        }
+    )
+    pwd = PasswordField(
+        label="管理员密码",
+        validators=[
+            DataRequired("请输入管理员密码！")
+        ],
+        description="管理员密码",
+        render_kw={
+            "class": "form-control",
+            "placeholder": "请输入管理员密码！",
+        }
+    )
+    repwd = PasswordField(
+        label="管理员重复密码",
+        validators=[
+            DataRequired("请输入管理员重复密码！"),
+            EqualTo('pwd', message="两次密码不一致！")
+        ],
+        description="管理员重复密码",
+        render_kw={
+            "class": "form-control",
+            "placeholder": "请输入管理员重复密码！",
+        }
+    )
+    role_id = SelectField(
+        label="所属角色",
+        coerce=int,
+        choices=[(v.id, v.name) for v in role_all],
+        render_kw={
+            "class": "form-control",
+        }
+    )
+    submit = SubmitField(
+        '编辑',
+        render_kw={
+            "class": "btn btn-primary",
+        }
+    )
+
+
+
